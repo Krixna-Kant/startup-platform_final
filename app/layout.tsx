7936027a8154fff642a33bcb016bf41a3ba671d3
Krixna-Kant <kant19krishna@gmail.com>
2025-03-18 00:38:58 +0530

    Title name chnaged

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Startup Validator',
+  title: 'Startup AI',
   description: 'Created with create-next-stack',
   generator: 'Krishna Kant',
 }
